4e9187040891e3d314d6ddb0ffbfcec70b061b32
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2020-08-20 13:52:06 +0530

    Add files via upload

---
+++ ExportSQL.sql
@@ -0,0 +1,14 @@
+/*script generated for free with OnlineDataGenerator available at: https://www.onlinedatagenerator.com */
+CREATE TABLE MOCKDATA(ID BIGINT,JobTitle VARCHAR(500),EmailAddress VARCHAR(500),FirstNameLastName VARCHAR(500));
+
+INSERT INTO MOCKDATA VALUES('1','Banker','Rae_Douglas2814@mafthy.com','Rae Douglas');
+INSERT INTO MOCKDATA VALUES('2','Pharmacist','Rick_Collins819@bulaffy.com','Rick Collins');
+INSERT INTO MOCKDATA VALUES('3','Staffing Consultant','Enoch_Tennant4530@typill.biz','Enoch Tennant');
+INSERT INTO MOCKDATA VALUES('4','Doctor','Brad_Slater6495@corti.com','Brad Slater');
+INSERT INTO MOCKDATA VALUES('5','Audiologist','Tony_Scott7463@dionrab.com','Tony Scott');
+INSERT INTO MOCKDATA VALUES('6','Lecturer','Doug_Alcroft8884@muall.tech','Doug Alcroft');
+INSERT INTO MOCKDATA VALUES('7','Accountant','Aiden_Craig5134@supunk.biz','Aiden Craig');
+INSERT INTO MOCKDATA VALUES('8','Project Manager','Ramon_Miller807@mafthy.com','Ramon Miller');
+INSERT INTO MOCKDATA VALUES('9','Paramedic','Maxwell_Sheldon6010@infotech44.tech','Maxwell Sheldon');
+INSERT INTO MOCKDATA VALUES('10','HR Coordinator','Mark_Bloom5672@qater.org','Mark Bloom');
+
